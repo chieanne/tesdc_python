3d8c875dd89d0b759b179271dbfdfa6c4db0c703
mich <mdmonton@gmail.com>
2022-10-01 09:57:21 +0800

    commit pythonactivity2

---
+++ src/pythonactivity2.py
@@ -0,0 +1,4 @@
+grade1 = input("Enter Grade in Math: ")
+grade2 = input("Enter Grade in English: ")
+grade3 = input("Enter Grade in Filipino: ")
+print(f'Average Grade is {(int(grade1)+int(grade2)+int(grade3)) / 3 }')
